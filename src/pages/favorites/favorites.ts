2d5ccda67f797156976f016693549092705196db
Daniel Lessa <daniellsf@id.uff.br>
2018-10-17 11:07:12 -0300

    Favorito funcionando

--- src/pages/favorites/favorites.ts
+++ src/pages/favorites/favorites.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, Platform } from 'ionic-angular';
 import { Http } from '@angular/http';
 import { Storage } from '@ionic/storage';
 import { TopicShowPage } from '../topics/topic-show/topic-show';
@@ -19,23 +19,29 @@ export class FavoritesPage {
     public navCtrl: NavController, 
     public navParams: NavParams,
     public http: Http,
-    public storage: Storage) {
+    public storage: Storage,
+    public platform: Platform) {
+      if (this.platform.is("cordova")){
+        this.URL_da_API = "https://hackaton-api.herokuapp.com/"
+      }
   }
 
 
   ionViewWillLoad(){
-    this.storage.get('user').then((val) => {this.user = val; });
+    //this.storage.get('user').then((val) => {this.user = val; });
     this.user = this.navParams.get('user');
+    console.log(this.user);
+    this.getFavorites();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FavoritesPage');
-    this.getFavorites();
   }
 
   getFavorites(){
     this.http.get(this.URL_da_API+"users/" + this.user.id + "/liked_topics")
     .subscribe(dados => {
+      console.log(dados);
       this.favorites = dados.json();
       return dados;
     }, e => {
